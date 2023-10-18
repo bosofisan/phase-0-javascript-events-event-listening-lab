41e67becbb0a015d1444ba34cdf360168049fbd4
lulu osofisan <boluosofisan@gmail.com>
2023-10-18 10:20:37 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+  const button = document.getElementById('button');
+
+  if(button) {
+    button.addEventListener('click', function() {
+      console.log('Button clicked!');
+    });
+  } else {
+    console.log('Button element not found.');
+  }
 }
